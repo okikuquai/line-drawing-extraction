01eff3b30d6af89ea85938a6c4da2abe3f4f28cd
Yuki Kikuchi <yuki-kikuchi@mail.nissan.co.jp>
2023-11-19 20:26:51 +0900

    add hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,71 @@
 "use client";
 
-import React from "react";
 import { InboxOutlined } from "@ant-design/icons";
 import type { UploadProps } from "antd";
 import { message, Upload } from "antd";
+import React, { useState, useEffect } from "react";
 
 const { Dragger } = Upload;
 
-//ここをいじればいいはず
-//https://ant.design/components/upload#api
-const handleBeforeUpload = (file: RcFile, fileList: RcFile[]) => {
-  const reader = new FileReader();
-  reader.onload = (e: ProgressEvent<FileReader>) => {
-    //多分ファイルの生データが入ってる
-    console.log("File content:", e.target?.result);
+const App: React.FC = () => {
+  const [fileContent, setFileContent] = useState<string | null>(null);
+
+  //ここをいじればいいはず
+  //https://ant.design/components/upload#api
+  const handleBeforeUpload = (file: RcFile, fileList: RcFile[]) => {
+    const reader = new FileReader();
+    reader.onload = (e: ProgressEvent<FileReader>) => {
+      //多分ファイルの生データが入ってる
+      console.log("File content:", e.target?.result);
+    };
+    setFileContent("終了しました！");
+    message.info(`${file.name} file read successfully`);
+
+    // ファイルをサーバーにアップロードしない
+    return false;
   };
-  message.info(`${file.name} file read successfully`);
 
-  // ファイルをサーバーにアップロードしない
-  return false;
+  const props: UploadProps = {
+    beforeUpload: handleBeforeUpload,
+    name: "file",
+    multiple: false,
+    onDrop(e: { dataTransfer: { files: any } }) {
+      console.log("Dropped files", e.dataTransfer.files);
+    },
+  };
+
+  useEffect(() => {
+    if (fileContent !== null) {
+      //画像の処理が終わったらここにhookする
+      message.success("終了しました！");
+    }
+  }, [fileContent]); // 依存配列に fileContent を指定
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <h1>{fileContent ?? "ファイルをアップロードしてください"}</h1>
+      <Dragger {...props}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">
+          Click or drag file to this area to upload
+        </p>
+        <p className="ant-upload-hint">
+          Support for a single or bulk upload. Strictly prohibited from
+          uploading company data or other banned files.
+        </p>
+      </Dragger>
+    </div>
+  );
 };
-
-const props: UploadProps = {
-  beforeUpload: handleBeforeUpload,
-  name: "file",
-  multiple: false,
-  onDrop(e: { dataTransfer: { files: any } }) {
-    console.log("Dropped files", e.dataTransfer.files);
-  },
-};
-
-const App: React.FC = () => (
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-    }}
-  >
-    <Dragger {...props}>
-      <p className="ant-upload-drag-icon">
-        <InboxOutlined />
-      </p>
-      <p className="ant-upload-text">
-        Click or drag file to this area to upload
-      </p>
-      <p className="ant-upload-hint">
-        Support for a single or bulk upload. Strictly prohibited from uploading
-        company data or other banned files.
-      </p>
-    </Dragger>
-  </div>
-);
 
 export default App;
